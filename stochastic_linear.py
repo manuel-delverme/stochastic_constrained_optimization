a1e25400713c86513cb52ccc40d7e41edce28bc8
manuel <manuel.delverme@gmail.com>
2021-08-04 04:05:31 +0200

    example 24 for epsilon = 0

--- stochastic_linear.py
+++ stochastic_linear.py
@@ -8,11 +8,14 @@ import tqdm
 
 n = d = 100
 eta = 0.2
-loss_scale = 0.05
-ITERS = 5000
+loss_scale = 0.
+constr_scale = 1.
+ITERS = 10000
 
 
 def stochastic_linear_constraint(tb):
+    plot_every = ITERS // 1000
+
     x = torch.ones((d, 1), requires_grad=True)
 
     A = torch.zeros(n, d, d)
@@ -22,16 +25,15 @@ def stochastic_linear_constraint(tb):
     def closure_(batch_index):
         Ai = A[batch_index].squeeze(0)
 
-        loss_i = Ai.matmul(x)
-
-        obj_i = loss_scale * loss_i.pow(2).sum()
-        constr_i = x.T.matmul(Ai)
+        # loss_i = Ai.matmul(x)
+        obj_i = 0.  # loss_scale * loss_i.pow(2).sum()
+        constr_i = constr_scale * x.T.matmul(Ai)
 
         return obj_i, [constr_i, ], None
 
     optimizer = torch_constrained.ConstrainedOptimizer(
-        torch.optim.SGD,
-        torch.optim.SGD,
+        torch_constrained.ExtraSGD,
+        torch_constrained.ExtraSGD,
         lr_x=eta,
         lr_y=eta,
         primal_parameters=[x, ],
@@ -43,29 +45,34 @@ def stochastic_linear_constraint(tb):
         closure = lambda: closure_(batch_index)
 
         lagr = optimizer.step(closure)
-        obj, (defect,), _ = closure()
 
-        dual = optimizer.equality_multipliers[0].weight
+        if (step % plot_every) == 0:
+            dual = optimizer.equality_multipliers[0].weight
+            for idx in range(10):
+                tb.add_scalar(f"{idx}/primal", float(x[idx]), step)
+                tb.add_scalar(f"{idx}/dual", float(dual[:, idx]), step)
 
-        tb.add_scalar(f"primal/obj", float(obj), step)
+                tb.add_scalar(f"{idx}/grad_primal", float(x.grad[idx]), step)
+                tb.add_scalar(f"{idx}/grad_dual", float(dual.grad[:, idx]), step)
+                tb.add_scalar(f"{idx}/selected", float(batch_index == idx), step)
 
-        tb.add_histogram(f"primal/x", x.detach().numpy(), step)
-        tb.add_scalar(f"primal/mean_x", float(x.mean()), step)
-        tb.add_scalar(f"primal/mean_abs_x", float(x.mean()), step)
+            obj, (defect,), _ = closure()
 
-        tb.add_scalar(f"dual/mean_defect", float(defect.mean()), step)
-        tb.add_scalar(f"dual/mean_abs_defect", float(defect.abs().mean()), step)
+            tb.add_scalar(f"primal/obj", float(obj), step)
 
-        tb.add_histogram(f"dual/lambda", dual.detach().numpy(), step)
-        tb.add_scalar(f"dual/mean_lambda", float(dual.mean()), step)
-        tb.add_scalar(f"dual/mean_abs_lambda", float(dual.abs().mean()), step)
+            tb.add_histogram(f"primal/x", x.detach().numpy(), step)
+            tb.add_scalar(f"primal/mean_x", float(x.mean()), step)
+            tb.add_scalar(f"primal/mean_abs_x", float(x.mean()), step)
 
-        tb.add_scalar(f"train/lagr", float(lagr), step)
-        tb.add_histogram("train/indices", batch_index, step)
+            tb.add_scalar(f"dual/mean_defect", float(defect.mean()), step)
+            tb.add_scalar(f"dual/mean_abs_defect", float(defect.abs().mean()), step)
 
-        for idx in range(10):
-            tb.add_scalar(f"{idx}/primal", float(x[idx]), step)
-            tb.add_scalar(f"{idx}/dual", float(dual[:, idx]), step)
+            tb.add_histogram(f"dual/lambda", dual.detach().numpy(), step)
+            tb.add_scalar(f"dual/mean_lambda", float(dual.mean()), step)
+            tb.add_scalar(f"dual/mean_abs_lambda", float(dual.abs().mean()), step)
+
+            tb.add_scalar(f"train/lagr", float(lagr), step)
+            tb.add_histogram("train/indices", batch_index, step)
 
         gradnorm.append(x.grad.abs().sum() + optimizer.equality_multipliers[0].weight.grad.abs().sum())
 

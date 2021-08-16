4dd4eb7a5591c6b9ba9ff1133e7eae5c8ede8f4e
manuel <manuel.delverme@gmail.com>
2021-08-16 18:18:53 +0200

    refactor/cleanup

--- stochastic_linear.py
+++ stochastic_linear.py
@@ -10,11 +10,11 @@ n = d = 100
 eta = 0.2
 loss_scale = 0.
 constr_scale = 1.
-ITERS = 10000
+iters = 10000
 
 
-def stochastic_linear_constraint(tb):
-    plot_every = ITERS // 1000
+def stochastic_linear_constraint(logger):
+    plot_every = iters // 1000
 
     x = torch.ones((d, 1), requires_grad=True)
 
@@ -22,8 +22,8 @@ def stochastic_linear_constraint(tb):
     for idx in range(n):
         A[idx, idx, idx] = 1.
 
-    def closure_(batch_index):
-        Ai = A[batch_index].squeeze(0)
+    def closure_(batch_index_):
+        Ai = A[batch_index_].squeeze(0)
 
         # loss_i = Ai.matmul(x)
         obj_i = 0.  # loss_scale * loss_i.pow(2).sum()
@@ -38,48 +38,46 @@ def stochastic_linear_constraint(tb):
         lr_y=eta,
         primal_parameters=[x, ],
     )
-    gradnorm = collections.deque(maxlen=d)
+    grad_norm = collections.deque(maxlen=d)
 
-    for step in tqdm.trange(ITERS):
+    for step in tqdm.trange(iters):
         batch_index = torch.randint(0, A.shape[0], (1,))
         closure = lambda: closure_(batch_index)
 
-        lagr = optimizer.step(closure)
+        lagrangian = optimizer.step(closure)  # noqa
 
         if (step % plot_every) == 0:
             dual = optimizer.equality_multipliers[0].weight
             for idx in range(10):
-                tb.add_scalar(f"{idx}/primal", float(x[idx]), step)
-                tb.add_scalar(f"{idx}/dual", float(dual[:, idx]), step)
+                logger.add_scalar(f"{idx}/primal", float(x[idx]), step)
+                logger.add_scalar(f"{idx}/dual", float(dual[:, idx]), step)
 
-                tb.add_scalar(f"{idx}/grad_primal", float(x.grad[idx]), step)
-                tb.add_scalar(f"{idx}/grad_dual", float(dual.grad[:, idx]), step)
-                tb.add_scalar(f"{idx}/selected", float(batch_index == idx), step)
+                logger.add_scalar(f"{idx}/grad_primal", float(x.grad[idx]), step)
+                logger.add_scalar(f"{idx}/grad_dual", float(dual.grad[:, idx]), step)
+                logger.add_scalar(f"{idx}/selected", float(batch_index == idx), step)
 
             obj, (defect,), _ = closure()
 
-            tb.add_scalar(f"primal/obj", float(obj), step)
+            logger.add_scalar(f"primal/obj", float(obj), step)
 
-            tb.add_histogram(f"primal/x", x.detach().numpy(), step)
-            tb.add_scalar(f"primal/mean_x", float(x.mean()), step)
-            tb.add_scalar(f"primal/mean_abs_x", float(x.mean()), step)
+            logger.add_histogram(f"primal/x", x.detach().numpy(), step)
+            logger.add_scalar(f"primal/mean_x", float(x.mean()), step)
+            logger.add_scalar(f"primal/mean_abs_x", float(x.mean()), step)
 
-            tb.add_scalar(f"dual/mean_defect", float(defect.mean()), step)
-            tb.add_scalar(f"dual/mean_abs_defect", float(defect.abs().mean()), step)
+            logger.add_scalar(f"dual/mean_defect", float(defect.mean()), step)
+            logger.add_scalar(f"dual/mean_abs_defect", float(defect.abs().mean()), step)
 
-            tb.add_histogram(f"dual/lambda", dual.detach().numpy(), step)
-            tb.add_scalar(f"dual/mean_lambda", float(dual.mean()), step)
-            tb.add_scalar(f"dual/mean_abs_lambda", float(dual.abs().mean()), step)
+            logger.add_histogram(f"dual/lambda", dual.detach().numpy(), step)
+            logger.add_scalar(f"dual/mean_lambda", float(dual.mean()), step)
+            logger.add_scalar(f"dual/mean_abs_lambda", float(dual.abs().mean()), step)
 
-            tb.add_scalar(f"train/lagr", float(lagr), step)
-            tb.add_histogram("train/indices", batch_index, step)
+            logger.add_scalar(f"train/lagrangian", float(lagrangian), step)
+            logger.add_histogram("train/indices", batch_index, step)
 
-        gradnorm.append(x.grad.abs().sum() + optimizer.equality_multipliers[0].weight.grad.abs().sum())
+        grad_norm.append(x.grad.abs().sum() + optimizer.equality_multipliers[0].weight.grad.abs().sum())
 
-        if sum(gradnorm) < 1e-10:
+        if sum(grad_norm) < 1e-10:
             break
-
-    # assert torch.allclose(x, torch.zeros_like(x))
 
 
 if __name__ == '__main__':
